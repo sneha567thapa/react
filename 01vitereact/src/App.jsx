6c0f8602711faa355c5a58c9d31d231dafd38449
sneha <snehaathapa60@gmail.com>
2026-03-09 23:55:57 +0545

    custom react added

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chain from "./chain"
+function App() {
+  const username = "sneha and code"
+
+  return (
+    <>
+   <Chain/>
+   <h1>sneha and react {username}</h1>
+   <p>test para</p>
+   </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/chain.jsx
@@ -0,0 +1,6 @@
+function Chain(){
+    return(
+        <h1>React is in progress</h1>
+    )
+}
+export default Chain
